e8a4c0626b036eb12d96e43ba2c9bd38c3633b50
ricardojunior02 <ricardojunior199502@gmail.com>
2021-04-05 11:07:11 -0300

    Componente de loading

---
+++ src/components/Loading/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+function Loading() {
+  return (
+    <div className="container">
+      <div className="loading"></div>
+    </div> 
+  );
+}
+
+export default Loading;
