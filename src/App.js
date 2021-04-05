689af09214f099432f301bdbcc5133569e769098
ricardojunior02 <ricardojunior199502@gmail.com>
2021-04-05 11:12:24 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Routes from './routes';
+
+import './App.css'
+
+import { Authenticate } from './context/authenticate';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticate>
+        <Routes />
+    </Authenticate>
   );
 }
 
